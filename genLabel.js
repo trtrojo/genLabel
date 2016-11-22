d67c8b46aa874c03cb19e6edcdad14f2903a452f
rew0rk <rew0rk@dodgeJunction.local.n-systems.us>
2016-11-21 23:02:16 -0500

    it works

--- genLabel.js
+++ genLabel.js
@@ -14,6 +14,7 @@ var fs = require("fs"); //writing to /tmp
 var path = require("path"); //apparently i need __dirname, no relative roots
 var PDFDocument = require("pdfkit");
 var randToken = require("rand-token");
+var barcode = require('barcode');
 
 var app = express();
 
@@ -48,26 +49,59 @@ app.use("/", express.static("./views"));
 
 app.get("/genLabel", function(req,res) {
  console.log("/genLabel query:", req.query);
+
+// if (typeof req.query.barcode !== 'undefined') {
+  //make sure to provide barcode data
+  var c128Image = barcode('code128', {
+   data:"it works",
+   width:400,
+   height:30
+  });
+  c128Image.saveImage("/tmp/barcode.PNG", function(err){
+   if (err) throw err;
+   console.log("PLEASE FOR THE LOVE OF GOD FIX THE PROBLEM ON LINE 62");
+  });
+// }
+
+
  doc = new PDFDocument(
  { //72.00 - 1 inch, 108.00 - 1.5 inch?
   size:[108.00,72.00],
-  margin:10
+  margin:1
  }
 );
 
 if (typeof req.query.fontSize !== 'undefined') {
  doc.fontSize(req.query.fontSize);
 } else {
-doc.fontSize(18);
+doc.fontSize(16);
 }
+  doc.font('Helvetica')
+  doc.text(req.query.line1, { //First line
+  align:'left'
+ });
+ doc.text(req.query.line2, {
+  align:'left'
+ });
+ doc.text(req.query.line3, {
+  align: 'left'
+ });
+ doc.image(
+  /*c128Image.getStream(
+   function(err,readStream) { 
+     if (err){throw err;}
+     readStream.pipe(CdnWriteStream); //todo: figure out how not to write image to disk
+    })*/ //this ends the getStream function.
 
-  doc.text(req.query.docTitle, { //First line
-  align:'center'
+  '/tmp/barcode2.PNG',{scale:0.25}
+ );
+ doc.fontSize(10).text(req.query.line5,{
+  align: 'center'
  });
- doc.text(req.query.docPayload, {
-  align:'center'
- });
- doc.pipe(res);
+ 
+
+
+doc.pipe(res);
  doc.end();
 
 });
